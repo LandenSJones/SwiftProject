a3efc1924112c9dfd6af487b653fb7aaad0b21ea
Landen Jones <59664899+LandenSJones@users.noreply.github.com>
2021-10-31 18:16:49 -0500

    Basic format Xcode provides

---
+++ SwiftProject/SwiftProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftProject
+//
+//  Created by Landen Jones on 10/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftProject/SwiftProject/SwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftProjectApp.swift
+//  SwiftProject
+//
+//  Created by Landen Jones on 10/31/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
